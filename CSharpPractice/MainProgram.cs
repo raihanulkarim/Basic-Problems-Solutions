f8b942d3968b3a95996fb60ee270fdef9c207409
RaihanulKarim <raihanulkarim5@gmail.com>
2021-12-15 11:13:39 +0600

    Add project files.

---
+++ CSharpPractice/MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+    public class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Problem1 problem1 = new Problem1();
+        }
+    }
+}
---
+++ CSharpPractice/Problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Problem1
+    {
+        public Problem1()
+        {
+            Console.WriteLine("Enter a Character");
+            char userVal = Convert.ToChar(Console.ReadLine());
+            int aschiVal = Convert.ToInt32(userVal);
+            if(aschiVal <= 90 && aschiVal >= 65)
+            {
+                Console.WriteLine(Convert.ToChar(aschiVal + 32));
+            }else if (aschiVal >= 97 && aschiVal <= 122)
+            {
+                Console.WriteLine(Convert.ToChar(aschiVal - 32));
+            }
+        }
+    }
+}
