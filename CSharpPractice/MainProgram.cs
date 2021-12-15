ee513c49ae00bf28451f4cc54f8d0c0f1e91d3ba
RaihanulKarim <raihanulkarim5@gmail.com>
2021-12-15 14:34:22 +0600

    Problem6 added

--- CSharpPractice/MainProgram.cs
+++ CSharpPractice/MainProgram.cs
@@ -10,7 +10,8 @@ namespace CSharpPractice
             //Problem2 problem2 = new Problem2();
             //Problem3 problem3 = new Problem3();
             //Problem4 problem4 = new Problem4();
-            Problem5 problem5 = new Problem5(); 
+            // Problem5 problem5 = new Problem5(); 
+            Problem6 problem6 = new Problem6();
         }
     }
 }
---
+++ CSharpPractice/Problem6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Problem6
+    {
+        public Problem6()
+        {
+            Console.WriteLine("Enter Year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("Leap Year!");
+            }
+            else
+            {
+                if (year % 100 == 0)
+                {
+                    Console.WriteLine("Not a Leapyear!");
+                }else if (year % 4 == 0)
+                {
+                    Console.WriteLine("Leap Year!");
+                }
+                else
+                {
+                    Console.WriteLine("Not a leapyear");
+                }
+            }
+        }
+    }
+}
