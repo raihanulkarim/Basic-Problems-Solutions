508c2ba5c07959205312719f4994aec215866cc3
RaihanulKarim <raihanulkarim5@gmail.com>
2021-12-15 15:17:34 +0600

    Problem10 added

--- CSharpPractice/MainProgram.cs
+++ CSharpPractice/MainProgram.cs
@@ -14,7 +14,8 @@ namespace CSharpPractice
             //Problem6 problem6 = new Problem6();
             //Problem7 problem7 = new Problem7();    
             //Problem8 problem8 = new Problem8();
-            Problem9 problem9  = new Problem9();
+            //Problem9 problem9  = new Problem9();
+            Problem10 problem10 = new Problem10();
         }
     }
 }
---
+++ CSharpPractice/Problem10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Problem10
+    {
+        public Problem10()
+        {
+            Console.WriteLine("Enter value:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
