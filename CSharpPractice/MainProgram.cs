cb3d57405b07b78c39f4b88b77b75ed8fa50b24f
RaihanulKarim <raihanulkarim5@gmail.com>
2021-12-15 12:01:33 +0600

    Problem 3 added

--- CSharpPractice/MainProgram.cs
+++ CSharpPractice/MainProgram.cs
@@ -7,7 +7,8 @@ namespace CSharpPractice
         static void Main(string[] args)
         {
             // Problem1 problem1 = new Problem1();
-            Problem2 problem2 = new Problem2();
+            //Problem2 problem2 = new Problem2();
+            Problem3 problem3 = new Problem3();
         }
     }
 }
---
+++ CSharpPractice/Problem3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Problem3
+    {
+        public Problem3()
+        {
+            Console.WriteLine("Enter x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter radius");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int coOrdinates = (x * x) + (y * y);
+            int radius_square = r * r;
+            if(coOrdinates == radius_square)
+            {
+                Console.WriteLine("On the Circle!");
+            }else if (coOrdinates < radius_square)
+            {
+                Console.WriteLine("Inside the Circle!");
+            }
+            else
+            {
+                Console.WriteLine("Outside the circle!"); 
+            }
+        }
+    }
+}
